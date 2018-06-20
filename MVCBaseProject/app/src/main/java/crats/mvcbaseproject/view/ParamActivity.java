d6aa7ef9482bd08ae92dcb3504fcf4610afd4130
paramjot <paramjot.to.you@gmail.com>
2018-06-19 20:33:52 -0400

    add my screen

---
+++ MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/ParamActivity.java
@@ -0,0 +1,15 @@
+package crats.mvcbaseproject.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import crats.mvcbaseproject.R;
+
+public class ParamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_param);
+    }
+}
